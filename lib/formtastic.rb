56773bb21db0ed85d3e749b26d836a18b45aa4c5
beerlington <pete@lette.us>
2012-01-27 09:53:08 -0500

    Fixed namespace for Rails 3.2

--- lib/formtastic.rb
+++ lib/formtastic.rb
@@ -1926,7 +1926,7 @@ module Formtastic #:nodoc:
           options = args.extract_options!
           options[:builder] ||= @@builder
           options[:html] ||= {}
-          @@builder.custom_namespace = options[:namespace].to_s
+          @@builder.custom_namespace = options.delete(:namespace).to_s
 
           singularizer = defined?(ActiveModel::Naming.singular) ? ActiveModel::Naming.method(:singular) : ActionController::RecordIdentifier.method(:singular_class_name)
 
